dae38349757bd715371602dbd901659a20b1c86b
HauLe-DA <legiahau071101@gmail.com>
2025-04-26 17:43:25 +0300

    Update DAY9-baitap.sql

--- DAY9-baitap.sql
+++ DAY9-baitap.sql
@@ -1,1 +1,10 @@
+-- BAI TAP 1: https://datalemur.com/questions/laptop-mobile-viewership
+
+-- BAI TAP 2: http://leetcode.com/problems/triangle-judgement/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- BAI TAP 3: https://datalemur.com/questions/uncategorized-calls-percentage
+
+-- BAI TAP 4: https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- BAI TAP 5: https://platform.stratascratch.com/coding/9881-make-a-report-showing-the-number-of-survivors-and-non-survivors-by-passenger-class?code_type=1
 
