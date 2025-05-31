a096105d100b8dabbbd5b1bcc079ec9beb0941f6
HauLe-DA <legiahau071101@gmail.com>
2025-06-01 00:40:24 +0300

    Update DAY13_baitap.sql

--- DAY13_baitap.sql
+++ DAY13_baitap.sql
@@ -47,7 +47,39 @@ COUNT(number) AS policy_holder_count
 FROM number_policy_holder;
 
 --EX4: https://datalemur.com/questions/sql-page-with-no-likes
+
+SELECT page_id
+FROM pages
+WHERE page_id NOT IN (
+SELECT DISTINCT page_id
+FROM page_likes
+)
+ORDER BY page_id DESC;
+
 --EX5: https://datalemur.com/questions/user-retention
+WITH june_users AS(
+SELECT user_id, 
+COUNT( user_id) AS active_users	  
+FROM user_actions
+WHERE event_type IN ('sign-in','like','comment')
+AND EXTRACT(MONTH FROM event_date) =6 
+AND EXTRACT(YEAR FROM event_date) = 2022
+GROUP BY user_id),
+july_users AS(
+SELECT user_id, 
+COUNT( user_id) AS active_users	  
+FROM user_actions
+WHERE event_type IN ('sign-in','like','comment')
+AND EXTRACT(MONTH FROM event_date) =7 
+AND EXTRACT(YEAR FROM event_date) = 2022
+GROUP BY user_id)
+SELECT
+7 AS mth, 
+COUNT(*) AS monthly_active_users
+FROM july_users AS a 
+JOIN june_users AS b 
+ON a.user_id=b.user_id;
+
 --EX6: https://leetcode.com/problems/monthly-transactions-i/?envType=study-plan-v2&envId=top-sql-50
 --EX7: https://leetcode.com/problems/product-sales-analysis-iii/?envType=study-plan-v2&envId=top-sql-50
 --EX8: https://leetcode.com/problems/customers-who-bought-all-products/?envType=study-plan-v2&envId=top-sql-50
