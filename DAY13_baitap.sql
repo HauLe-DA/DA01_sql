13013ead972b8f25e94fa3ddf8564ffc3e37d580
HauLe-DA <legiahau071101@gmail.com>
2025-05-31 21:10:43 +0300

    Update DAY13_baitap.sql

--- DAY13_baitap.sql
+++ DAY13_baitap.sql
@@ -8,7 +8,44 @@ GROUP BY company_id, title, description
 ) AS job_count_cte
 WHERE job_count > 1;
 --EX2: https://datalemur.com/questions/sql-highest-grossing
+WITH total_appliance
+AS (
+SELECT  category, product, 
+SUM(spend	) AS total_spend
+FROM product_spend
+WHERE category='appliance' AND EXTRACT(YEAR FROM transaction_date) = 2022 
+GROUP BY product, category
+ORDER BY category, total_spend DESC
+LIMIT 2)
+,
+total_electronics
+AS (
+SELECT category, product, 
+SUM(spend	) AS total_spend
+FROM product_spend
+WHERE category='electronics' AND EXTRACT(YEAR FROM transaction_date) = 2022 
+GROUP BY product, category
+ORDER BY category, total_spend DESC
+LIMIT 2)
+  
+SELECT*
+FROM total_appliance
+UNION ALL 
+SELECT * 
+FROM total_electronics;
 --EX3: https://datalemur.com/questions/frequent-callers
+
+WITH number_policy_holder
+AS
+(SELECT policy_holder_id,
+COUNT(case_id) AS number
+FROM callers
+GROUP BY policy_holder_id
+HAVING COUNT(case_id) >=3)
+SELECT
+COUNT(number) AS policy_holder_count
+FROM number_policy_holder;
+
 --EX4: https://datalemur.com/questions/sql-page-with-no-likes
 --EX5: https://datalemur.com/questions/user-retention
 --EX6: https://leetcode.com/problems/monthly-transactions-i/?envType=study-plan-v2&envId=top-sql-50
