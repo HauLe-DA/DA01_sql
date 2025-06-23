e5140012ee58a3f786b3f4f739d51e54712b6981
HauLe-DA <legiahau071101@gmail.com>
2025-06-23 14:52:28 +0300

    Create DAY15-baitap.sql

---
+++ DAY15-baitap.sql
@@ -0,0 +1,9 @@
+ex1: datalemur-yoy-growth-rate: https://datalemur.com/questions/yoy-growth-rate 
+
+ex2: datalemur-card-launch-success: https://datalemur.com/questions/card-launch-success 
+ex3: datalemur-third-transaction: https://datalemur.com/questions/sql-third-transaction 
+ex4: datalemur-histogram-users-purchases: https://datalemur.com/questions/histogram-users-purchases 
+ex5: datalemur-rolling-average-tweets: https://datalemur.com/questions/rolling-average-tweets 
+ex6: datalemur-repeated-payments: https://datalemur.com/questions/repeated-payments 
+ex7: datalemur-highest-grossing: https://datalemur.com/questions/sql-highest-grossing 
+ex8: datalemur-top-fans-rank: https://datalemur.com/questions/top-fans-rank
