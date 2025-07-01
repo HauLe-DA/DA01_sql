e6b40d353c552ddec22ccec37433022abdc5d803
HauLe-DA <legiahau071101@gmail.com>
2025-07-01 11:04:39 +0300

    Update DAY15-baitap.sql

--- DAY15-baitap.sql
+++ DAY15-baitap.sql
@@ -1,5 +1,17 @@
 ex1: datalemur-yoy-growth-rate: https://datalemur.com/questions/yoy-growth-rate 
-
+WITH curr_year_transactions AS(
+SELECT 
+EXTRACT(YEAR FROM transaction_date) AS year,
+product_id,
+SUM(spend) OVER(PARTITION BY product_id, EXTRACT(YEAR FROM transaction_date)) AS curr_year_spend
+FROM user_transactions)
+SELECT year,
+product_id,
+curr_year_spend,
+LAG(curr_year_spend) OVER(PARTITION BY product_id) AS prev_year_spend,
+ROUND(((curr_year_spend/LAG(curr_year_spend) OVER(PARTITION BY product_id))-1)*100.0,2) AS yoy_rate
+FROM curr_year_transactions
+;
 ex2: datalemur-card-launch-success: https://datalemur.com/questions/card-launch-success 
 ex3: datalemur-third-transaction: https://datalemur.com/questions/sql-third-transaction 
 ex4: datalemur-histogram-users-purchases: https://datalemur.com/questions/histogram-users-purchases 
