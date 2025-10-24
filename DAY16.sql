b7ca60b26b3cd2a68349751982e4784060369668
HauLe-DA <legiahau071101@gmail.com>
2025-10-24 23:12:09 +0300

    Create DAY16.sql

---
+++ DAY16.sql
@@ -0,0 +1,10 @@
+Leetcode 🚀
+ex1 leetcode-mmediate-food-delivery httpsleetcode.comproblemsimmediate-food-delivery-iienvType=study-plan-v2&envId=top-sql-50
+  
+ex2 leetcode-game-play-analysis httpsleetcode.comproblemsgame-play-analysis-ivenvType=study-plan-v2&envId=top-sql-50
+ex3 leetcode-exchange-seats httpsleetcode.comproblemsexchange-seatsenvType=study-plan-v2&envId=top-sql-50
+ex4 leetcode-restaurant-growth httpsleetcode.comproblemsrestaurant-growthenvType=study-plan-v2&envId=top-sql-50
+ex5 leetcode-investments-in-2016 httpsleetcode.comproblemsinvestments-in-2016envType=study-plan-v2&envId=top-sql-50
+ex6 leetcode-department-top-three-salaries httpsleetcode.comproblemsdepartment-top-three-salariesenvType=study-plan-v2&envId=top-sql-50
+ex7 leetcode-last-person-to-fit-in-the-bus httpsleetcode.comproblemslast-person-to-fit-in-the-busenvType=study-plan-v2&envId=top-sql-50
+ex8 leetcode-product-price-at-a-given-date httpsleetcode.comproblemsproduct-price-at-a-given-dateenvType=study-plan-v2&envId=top-sql-50
